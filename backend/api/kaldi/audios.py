771dd03fabef9d4ce619fbb98c8b6fc2f158f5f3
wangshujingscan <wangshujing_123@163.com>
2019-07-15 16:01:28 +0800

    修改单个文件识别，并解决中文乱码问题

--- backend/api/kaldi/audios.py
+++ backend/api/kaldi/audios.py
@@ -5,6 +5,10 @@
 import sys
 import subprocess
 import platform
+import io
+import sys
+#改变标准输出的默认编码
+sys.stdout=io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
 
 class audio:
     def __init__(self,filepath):
@@ -31,8 +35,8 @@ class audio:
             '--word-symbol-table=backend/api/kaldi/aidatatang_200zh/s5/data/lang/words.txt '\
             'backend/api/kaldi/aidatatang_200zh/s5/exp/chain/tdnn_1a_sp/final.mdl '\
             'backend/api/kaldi/aidatatang_200zh/s5/exp/chain/tdnn_1a_sp/graph/HCLG.fst '\
-            'ark:backend/api/kaldi/aidatatang_200zh/s5/data/test/spk2utt_2 '\
-            'scp:backend/api/kaldi/aidatatang_200zh/s5/data/test/wav2_win.scp '\
+            'ark:backend/api/kaldi/aidatatang_200zh/s5/data/test/spk2utt_2_one '\
+            'scp:backend/api/kaldi/aidatatang_200zh/s5/data/test/wav2_win_one.scp '\
             'ark,t:backend/api/kaldi/aidatatang_200zh/s5/20190619.txt'
         elif(sysstr == "Linux"):
             cmd='online2-wav-nnet3-latgen-faster ' \
@@ -50,8 +54,8 @@ class audio:
             '--word-symbol-table=backend/api/kaldi/aidatatang_200zh/s5/data/lang/words.txt '\
             'backend/api/kaldi/aidatatang_200zh/s5/exp/chain/tdnn_1a_sp/final.mdl '\
             'backend/api/kaldi/aidatatang_200zh/s5/exp/chain/tdnn_1a_sp/graph/HCLG.fst '\
-            'ark:backend/api/kaldi/aidatatang_200zh/s5/data/test/spk2utt_2 '\
-            'scp:backend/api/kaldi/aidatatang_200zh/s5/data/test/wav2_dev.scp '\
+            'ark:backend/api/kaldi/aidatatang_200zh/s5/data/test/spk2utt_2_one '\
+            'scp:backend/api/kaldi/aidatatang_200zh/s5/data/test/wav2_dev_one.scp '\
             'ark,t:backend/api/kaldi/aidatatang_200zh/s5/20190619.txt'
         else:
             print ("No Support audios in this platform")
@@ -59,9 +63,19 @@ class audio:
         
         # cmd = 'dir'
 
-        retcode = subprocess.call(cmd,shell=True)
-        print (retcode)
-        return retcode
+        _, out = subprocess.getstatusoutput(cmd)
+        lines = out.split('\n')
+        index = 0
+        for line in lines:
+            if 'IOR' in line:
+                break
+            index += 1
+            # print ("line:%s %s\n" % (index,line))
+            if (index == 8):
+                output = line.split("T0055G0036S0002")[1].strip()
+                output_utf8 = output.encode('gbk').decode('utf-8')
+                print ("Result:%s" % output_utf8)
+                return output_utf8
 
 if __name__ == '__main__':
     myaudio = audio("test.wav")
