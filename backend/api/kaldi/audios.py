8f3211c24179e05f3490e162f9410143ebb22c83
wangshujingscan <wangshujing_123@163.com>
2019-07-15 10:01:37 +0800

    更新linux支持

--- backend/api/kaldi/audios.py
+++ backend/api/kaldi/audios.py
@@ -4,13 +4,19 @@
 """
 import sys
 import subprocess
+import platform
 
 class audio:
     def __init__(self,filepath):
         self.filepath = filepath
 
     def getAudioContent(self):
-        cmd='online2-wav-nnet3-latgen-faster.exe ' \
+
+        # print(platform.system())
+        sysstr = platform.system()
+
+        if(sysstr =="Windows"):
+            cmd='online2-wav-nnet3-latgen-faster.exe ' \
             '--config=backend/api/kaldi/aidatatang_200zh/s5/exp/chain/nnet_online/conf/online_win.conf ' \
             '--do-endpointing=false ' \
             '--frames-per-chunk=20 ' \
@@ -28,6 +34,28 @@ class audio:
             'ark:backend/api/kaldi/aidatatang_200zh/s5/data/test/spk2utt_2 '\
             'scp:backend/api/kaldi/aidatatang_200zh/s5/data/test/wav2_win.scp '\
             'ark,t:backend/api/kaldi/aidatatang_200zh/s5/20190619.txt'
+        elif(sysstr == "Linux"):
+            cmd='online2-wav-nnet3-latgen-faster ' \
+            '--config=backend/api/kaldi/aidatatang_200zh/s5/exp/chain/nnet_online/conf/online_dev.conf ' \
+            '--do-endpointing=false ' \
+            '--frames-per-chunk=20 ' \
+            '--extra-left-context-initial=0 '\
+            '--online=true '\
+            '--frame-subsampling-factor=3 '\
+            '--max-active=7000 '\
+            '--beam=15.0 '\
+            '--lattice-beam=6.0 '\
+            '--online=false '\
+            '--acoustic-scale=1.4 '\
+            '--word-symbol-table=backend/api/kaldi/aidatatang_200zh/s5/data/lang/words.txt '\
+            'backend/api/kaldi/aidatatang_200zh/s5/exp/chain/tdnn_1a_sp/final.mdl '\
+            'backend/api/kaldi/aidatatang_200zh/s5/exp/chain/tdnn_1a_sp/graph/HCLG.fst '\
+            'ark:backend/api/kaldi/aidatatang_200zh/s5/data/test/spk2utt_2 '\
+            'scp:backend/api/kaldi/aidatatang_200zh/s5/data/test/wav2_dev.scp '\
+            'ark,t:backend/api/kaldi/aidatatang_200zh/s5/20190619.txt'
+        else:
+            print ("No Support audios in this platform")
+            return False
         
         # cmd = 'dir'
 
