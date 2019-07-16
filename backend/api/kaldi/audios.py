65f068e241cc0756a17606f6f2fe7f207c021158
wangshujingscan <wangshujing_123@163.com>
2019-07-16 12:34:50 +0800

    修改支持传文件路径后识别

--- backend/api/kaldi/audios.py
+++ backend/api/kaldi/audios.py
@@ -9,10 +9,11 @@ import io
 import sys
 #改变标准输出的默认编码
 sys.stdout=io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
+import os
 
 class audio:
-    def __init__(self,filepath):
-        self.filepath = filepath
+    def __init__(self):
+        self = self
 
     def RunShellWithReturnCode(self,command):
         p = subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
@@ -26,7 +27,19 @@ class audio:
         # print(output)
         return output
 
-    def getAudioContent(self):
+    def getOneAudioContent(self, wavpath):
+        #copy filepath to scp file
+        if wavpath is None:
+            return False
+        
+        (path, tempfilename) = os.path.split(wavpath)
+        (filename, _) = os.path.splitext(tempfilename)
+        scpfilename = filename + ".scp"
+        scppath = os.path.join(path , scpfilename)
+
+        with open(scppath,'w+') as scpfile:
+            scpinfo = 'T0055G0036S0002 ' + wavpath
+            scpfile.write(scpinfo)
 
         # print(platform.system())
         sysstr = platform.system()
@@ -48,7 +61,7 @@ class audio:
             'backend/api/kaldi/aidatatang_200zh/s5/exp/chain/tdnn_1a_sp/final.mdl '\
             'backend/api/kaldi/aidatatang_200zh/s5/exp/chain/tdnn_1a_sp/graph/HCLG.fst '\
             'ark:backend/api/kaldi/aidatatang_200zh/s5/data/test/spk2utt_2_one '\
-            'scp:backend/api/kaldi/aidatatang_200zh/s5/data/test/wav2_win_one.scp '\
+            'scp:' + scppath + ' '\
             'ark,t:backend/api/kaldi/aidatatang_200zh/s5/20190619.txt'
         elif(sysstr == "Linux"):
             cmd='online2-wav-nnet3-latgen-faster ' \
@@ -67,7 +80,7 @@ class audio:
             'backend/api/kaldi/aidatatang_200zh/s5/exp/chain/tdnn_1a_sp/final.mdl '\
             'backend/api/kaldi/aidatatang_200zh/s5/exp/chain/tdnn_1a_sp/graph/HCLG.fst '\
             'ark:backend/api/kaldi/aidatatang_200zh/s5/data/test/spk2utt_2_one '\
-            'scp:backend/api/kaldi/aidatatang_200zh/s5/data/test/wav2_dev_one.scp '\
+            'scp:' + scppath + ' '\
             'ark,t:backend/api/kaldi/aidatatang_200zh/s5/20190619.txt'
         else:
             print ("No Support audios in this platform")
@@ -90,5 +103,9 @@ class audio:
                 return output
 
 if __name__ == '__main__':
-    myaudio = audio("test.wav")
-    myaudio.getAudioContent()
+    myaudio = audio()
+    # Windows
+    myaudio.getOneAudioContent("D:\\var\\www\\gallery\\media\\audios\\T0055G0036S0010.wav")
+
+    # Linux
+    # myaudio.getOneAudioContent("/var/www/gallery/media/audios/T0055G0036S0003.wav")
