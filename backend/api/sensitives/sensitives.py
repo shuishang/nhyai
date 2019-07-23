5fcbb634aaf35fe51c0efc81e9abfbf80a60d886
wangshujingscan <wangshujing_123@163.com>
2019-07-23 16:08:08 +0800

    修改敏感词函数

--- backend/api/sensitives/sensitives.py
+++ backend/api/sensitives/sensitives.py
@@ -15,8 +15,28 @@ import codecs
 import mmap
 import contextlib
 from django.conf import settings
+
 class sensitiveClass:
-    def check_sensitiveWords(keywords):
+    def __init__(self, df):
+        self = self
+        self.df = df
+
+    def check_sensitiveWords_test(self, keywords):
+        t = time.time()
+        startTime = int(round(t * 1000))
+        result = {}
+        for index, row in self.df.iterrows():
+            if keywords in row['内容']:
+                result["firstType"] = row['大类']
+                result["secondType"] = row['次类']
+                result["keyword"] = keywords
+                break
+        t = time.time()
+        endTime = int(round(t * 1000))
+        print(endTime - startTime)
+        print("result:",result)
+
+    def check_sensitiveWords(self, keywords):
         t = time.time()
         startTime = int(round(t * 1000))
         result = {}
@@ -25,42 +45,12 @@ class sensitiveClass:
                 result["firstType"] = row['大类']
                 result["secondType"] = row['次类']
                 result["keyword"] = keywords
-                break;
+                break
         t = time.time()
         endTime = int(round(t * 1000))
         print(endTime - startTime)
         print("result:",result)
-    if __name__ == '__main__':
-        check_sensitiveWords("十八摸")	 
-# def check_sensitiveWords(keywords):    
-#     pwd = os.getcwd()
-#     csvFile = os.path.join(pwd,"backend","api","sensitives","sensitiveWords.csv")
-#     #keywords="天安門屠殺"
-#     print ("keywords input: ", keywords)
-#     file =open(csvFile,'r')
-#     lines=file.readlines()
-#     file.close()
-#     row=[]#定义行数组
-#     column=[]#定义列数组
-#     rowNumber = 1
-#     result = {}
-#     t = time.time()
-#     startTime = int(round(t * 1000))
-#     for line in lines:
-#         if keywords in line:            
-#             arr = line.split(',')    
-#             print ("the keywords you entered belongs to the following data: ",keywords)
-#             result = {
-#                 "firstType": arr[0],
-#                 "secondType": arr[1],
-#                 "keyword": keywords
-#             }
-#         rowNumber = rowNumber + 1
-    
-#     t = time.time()
-#     endTime = int(round(t * 1000))
-#     result["millisecond"]=endTime-startTime
-#     print("result:",result)
-#     return result
-# if __name__ == '__main__':
-# 	check_sensitiveWords("十八摸")	 
+
+if __name__ == '__main__':
+    df = pd.read_csv(os.path.join(os.getcwd(),"backend","api","sensitives","sensitiveWords.csv"),encoding='gbk')
+    sensitiveClass(df).check_sensitiveWords_test("十八摸")
