a913f296241c502720b1de864df5de91ad9c50fa
xiongjian <xiongjian@hn-ssc.com>
2019-07-25 16:47:40 +0800

    修改敏感词返回方式

--- backend/api/sensitives/sensitives.py
+++ backend/api/sensitives/sensitives.py
@@ -23,28 +23,44 @@ class sensitiveClass:
     def check_sensitiveWords_test(self, df, input_word):
         t = time.time()
         startTime = int(round(t * 1000))
-        result = {}
+        
+        sensitive_hit_flag = 0 #0-五敏感词  1-有敏感词
+        sensitive_list = [] #敏感词列表
+        sensitive_size = 0    #敏感词数量
+
         if input_word != None:
             keywords = input_word.split(' ')
-
-        for index, row in df.iterrows():
-            sensitiveCms = row['内容'].split('、')
-            for keyword in keywords:
+        
+        for keyword in keywords:
+            print("33333333333=",keyword)
+            for index, row in df.iterrows():
+                sensitiveCms = row['内容'].split('、')
                 if keyword in sensitiveCms:
+                    result = {}
                     result["firstType"] = row['大类']
                     result["secondType"] = row['次类']
                     result["keyword"] = keyword
-                    break
+                    sensitive_hit_flag = 1
+                    sensitive_size = sensitive_size + 1
+                    sensitive_list.append(result)
+                    #break
         t = time.time()
         endTime = int(round(t * 1000))
         print(endTime - startTime)
-        print("result:",result)
-        return result
+        resultMap = {} 
+        resultMap["sensitive_hit_flag"] = sensitive_hit_flag
+        resultMap["sensitive_size"] = sensitive_size
+        resultMap["sensitive_list"] = sensitive_list
+        print("resultMap:",resultMap)
+        return resultMap
 
     def check_sensitiveWords(self, input_word):
         t = time.time()
         startTime = int(round(t * 1000))
-        result = {}
+        
+        sensitive_hit_flag = 0 #0-五敏感词  1-有敏感词
+        sensitive_list = [] #敏感词列表
+        sensitive_size = 0    #敏感词数量
         if input_word != None:
             keywords = input_word.split(' ')
         
@@ -52,17 +68,26 @@ class sensitiveClass:
             sensitiveCms = row['内容'].split('、')
             for keyword in keywords:
                 if keyword in sensitiveCms:
+                    result = {}
                     result["firstType"] = row['大类']
                     result["secondType"] = row['次类']
                     result["keyword"] = keyword
-                    break
+                    sensitive_hit_flag = 1
+                    sensitive_size = sensitive_size + 1
+                    sensitive_list.append(result)
+                    #break
         t = time.time()
         endTime = int(round(t * 1000))
         print(endTime - startTime)
-        print("result:",result)
-        return result
+        #print("result:",result)
+        resultMap = {} 
+        resultMap["sensitive_hit_flag"] = sensitive_hit_flag
+        resultMap["sensitive_size"] = sensitive_size
+        resultMap["sensitive_list"] = sensitive_list
+        print("resultMap:",resultMap)
+        return resultMap
 
 if __name__ == '__main__':
     df = pd.read_csv(os.path.join(os.getcwd(),"backend","api","sensitives","sensitiveWords.csv"),encoding='gbk')
-    sensitiveClass().check_sensitiveWords_test(df, "十八摸")
-    sensitiveClass().check_sensitiveWords_test(df, "你 不是 跟 我 讲的 笑话 吗")
+    #sensitiveClass().check_sensitiveWords_test(df, "十八摸")
+    sensitiveClass().check_sensitiveWords_test(df, "你 不是 跟 我 讲的 笑话 吗 欲死欲仙 十八摸")
