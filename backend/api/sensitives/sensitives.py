e6fc470561e320f1430ef2d0651f06ac3231fa03
xiongjian <xiongjian@hn-ssc.com>
2019-09-06 11:16:58 +0800

    修改敏感词算法

--- backend/api/sensitives/sensitives.py
+++ backend/api/sensitives/sensitives.py
@@ -31,6 +31,14 @@ class sensitiveClass:
             if text in nums:
                 nums.remove(text)  
         return nums
+    def bubble_sort_1(self,nums):
+        repeatedText = []
+        for i in range(len(nums)):  
+            for j in range(len(nums)): 
+                if i!=j and nums[i] in nums[j]:
+                    repeatedText.append(nums[i])
+        repeatedText = list(set(repeatedText)) 
+        return repeatedText
 
     def check_sensitiveWords_test(self, df, input_word):
         t = time.time()
@@ -94,24 +102,28 @@ class sensitiveClass:
         print("11111111111111   keyword_arr_sort=",keyword_arr_sort)
 
         removeRepeartedArra = sensitiveClass().bubble_sort(keyword_arr_sort)
+
+        removeRepeartedArra1 = sensitiveClass().bubble_sort_1(keyword_arr)
+        print("77777777777removeRepeartedArra1=",removeRepeartedArra1)
+
         print("22222222222   removeRepeartedArra=",removeRepeartedArra)
         web_text = input_word
         app_text = input_word
         for text in removeRepeartedArra:
-            web_text = web_text.replace(text, "<a style='color:red'>"+text+"</a>")
+            web_text = web_text.replace(text, "<span style='color:red'>"+text+"</span>")
             #web_text = input_word.replace(text, "<a style='color:red'>"+text+"</a>")
             app_text = app_text.replace(text, "<font color ='red' size='20'>"+text+"</font>")
-            for s in sensitive_list:
-                kw = s["keyword"]
-                if kw == text:                    
-                    sensitive_list_result.append(s)
-        #web_text = changeText
-        print("ccccccccccccc web_text=",web_text)
+        for text in removeRepeartedArra1:
+            web_text = web_text.replace(text, "<span style='color:red'>"+text+"</span>")
+            #web_text = input_word.replace(text, "<a style='color:red'>"+text+"</a>")
+            app_text = app_text.replace(text, "<font color ='red' size='20'>"+text+"</font>")    
+
         t = time.time()
         endTime = int(round(t * 1000))
         print(endTime - startTime)
         resultMap["sensitive_hit_flag"] = sensitive_hit_flag
-        resultMap["sensitive_size"] = len(sensitive_list_result)
+        #resultMap["sensitive_size"] = len(sensitive_list_result)
+        resultMap["sensitive_size"] = sensitive_size
         resultMap["sensitive_list"] = sensitive_list
         resultMap["final_list"] = aList
         print("aList=",aList)
@@ -214,7 +226,6 @@ class sensitiveClass:
                 if keyword.strip()=='':
                     break
                 if keyword in input_word:
-                    print("dddddddddddddddddd===",keyword)
                     result = {}
                     result["firstType"] = row['大类']
                     result["secondType"] = row['次类']
@@ -243,26 +254,30 @@ class sensitiveClass:
         aList = list(set(aList)) 
         keyword_arr_sort = list(set(keyword_arr))
         removeRepeartedArra = sensitiveClass().bubble_sort(keyword_arr_sort)
+        removeRepeartedArra1 = sensitiveClass().bubble_sort_1(keyword_arr)
         web_text = input_word
         app_text = input_word
         sensitive_list_result = []
         for text in removeRepeartedArra:
-            web_text = web_text.replace(text, "<a style='color:red'>"+text+"</a>")
+            web_text = web_text.replace(text, "<span style='color:red'>"+text+"</span>")
             app_text = app_text.replace(text, "<font color ='red' size='20'>"+text+"</font>")
             for s in sensitive_list:
                 kw = s["keyword"]
                 if kw == text:                    
                     sensitive_list_result.append(s)
+        for text in removeRepeartedArra1:
+            web_text = web_text.replace(text, "<span style='color:red'>"+text+"</span>")
+            app_text = app_text.replace(text, "<font color ='red' size='20'>"+text+"</font>")                    
         t = time.time()
         endTime = int(round(t * 1000))
         print(endTime - startTime)
         
-        resultMap["sensitive_list"] = sensitive_list_result
+        resultMap["sensitive_list"] = sensitive_list
         resultMap["final_list"] = aList
         print("aList=",aList)
         print("sensitive_list=",sensitive_list_result)
         resultMap["sensitive_hit_flag"] = sensitive_hit_flag
-        resultMap["sensitive_size"] = len(sensitive_list_result)
+        resultMap["sensitive_size"] = sensitive_size
         resultMap["web_text"] = "<div>"+web_text+"</div>"
         resultMap["app_text"] = app_text
         resultMap["takingTimes"] = endTime - startTime
@@ -327,7 +342,7 @@ class sensitiveClass:
 if __name__ == '__main__':
     df = pd.read_csv(os.path.join(os.getcwd(),"backend","api","sensitives","sensitiveWords.csv"),encoding='gbk')
     #sensitiveClass().check_sensitiveWords_test(df, "十八摸 11111")
-    sensitiveClass().check_sensitiveWords_test(df, " 十八摸  我是台独分子   台独分子  独分子 十八摸")
+    sensitiveClass().check_sensitiveWords_test(df, " 十八摸  我是台独分子   台独分子  独分子 台独 十八摸 台独共党")
     #sensitiveClass().check_sensitiveWords_test(df, "  heheh ")
     #sensitiveClass().check_sensitiveWords_test(df, "你 不是 跟 我 讲的 笑话 吗 欲死欲仙 十八摸")
     
