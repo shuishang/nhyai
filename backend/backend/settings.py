326a84f13f81b2499cbfea29c0c1931c45c57027
wangshujingscan <wangshujing_123@163.com>
2019-09-17 16:21:17 +0800

    解决视频文件滚动条不可拖动问题

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.http.ConditionalGetMiddleware',
+    'backend.rangesMiddleware.RangesMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
